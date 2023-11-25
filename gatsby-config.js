361a4f84523ec641d8a2d8e67c0537b37e041274
Niko Salminen <niko@salminen.me>
2023-11-25 17:30:48 +0200

    Add gtag configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,36 @@
 module.exports = {
   siteMetadata: {
     title: `Girls and the Gang`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.girlsandthegang.fi`,
   },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-TV21JG5FGJ", // Google Analytics / GA
+        ],
+      },
     },
-    __key: "images"
-  }]
+
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
