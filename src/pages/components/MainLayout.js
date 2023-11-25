a8c386baaff8e2c4c0f0a7a898d35c0b5d8d91d5
Niko Salminen <niko@salminen.me>
2023-11-25 20:35:41 +0200

    Fix styles

--- src/pages/components/MainLayout.js
+++ src/pages/components/MainLayout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {mainLayoutDiv } from "./MainLayout.module.css"
 
-const MainLayout = (props) => {
+const MainLayout = (props) => (
 
     <div className={mainLayoutDiv}>
         {props.children}
     </div>
 
-}
+)
 
 export default MainLayout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,78 +7,6 @@ import MainArticle from "./components/MainArticle"
 import Content from "./components/Content"
 import MainLayout from "./components/MainLayout"
 
-const pageStyles = {
-  color: "#232129",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-
 const IndexPage = () => {
   return (
     <MainLayout>
@@ -94,4 +22,4 @@ const IndexPage = () => {
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Girls and the Gang</title>
