20d55e912017b8c831257c27e58826153adce93f
Niko Salminen <niko@salminen.me>
2023-11-25 19:29:09 +0200

    Logo size and positioning

--- src/pages/components/Logo.js
+++ src/pages/components/Logo.js
@@ -3,9 +3,9 @@ import LogoSvg from "../../images/gg-logo.inline.svg"
 import { logo } from "./Logo.module.css"
 
 const Logo = () => (
-    <>
+    <a href="/">
         <LogoSvg className={logo} />
-    </>
+    </a>
 )
 
 export default Logo;
