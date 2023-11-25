17caac270f9fb7764a5d3e8c0dbece20d943ae58
Niko Salminen <niko@salminen.me>
2023-11-25 19:19:10 +0200

    Add logo in topbar

--- src/pages/components/Logo.js
+++ src/pages/components/Logo.js
@@ -1,8 +1,10 @@
 import React from "react";
+import LogoSvg from "../../images/gg-logo.inline.svg"
+import { logo } from "./Logo.module.css"
 
 const Logo = () => (
     <>
-        
+        <LogoSvg className={logo} />
     </>
 )
 
--- src/pages/components/TopBar.js
+++ src/pages/components/TopBar.js
@@ -1,10 +1,11 @@
 import React from "react"
 
 import { bar } from "./TopBar.module.css"
+import Logo from "./Logo";
 
 const TopBar = () => (
     <nav className={bar}>
-        Hello
+        <Logo />
     </nav>
 )
 
