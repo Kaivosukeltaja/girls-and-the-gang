3e1ff0a1bcc72cb185a376682aa0be4093f87abb
markusmasalin <user.name>
2023-12-15 14:23:00 +0200

    Footer component created

---
+++ src/pages/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import { footer } from "./Footer.module.css"
+import Logo from "./Logo";
+
+
+
+const Footer = () => (
+    <footer className={footer}>
+        <Logo />
+
+    </footer>
+  );
+  
+  export default Footer;
